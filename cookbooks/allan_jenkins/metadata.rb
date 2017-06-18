ea477682c664f88a956effda9e794e649e9a9e60
Allan Espinosa <allan.espinosa@outlook.com>
2017-06-17 22:51:48 -0400

    Upgrade the jenkins cookbook
    Remove the implicit dependency from jenkins 2.x' transitive dependency
    apt and depent directly

--- cookbooks/allan_jenkins/metadata.rb
+++ cookbooks/allan_jenkins/metadata.rb
@@ -8,7 +8,8 @@ source_url 'https://github.com/aespinosa/cookbooks'
 maintainer 'Allan Espinosa'
 maintainer_email 'allan.espinosa@outlook.com'
 
-depends 'jenkins', '~> 2.6.0'
+depends 'jenkins', '~> 5.0'
+depends 'apt', '~> 6.1'
 
 gem 'gcloud', '~> 0.12.2'
 gem 'acme-client', '~> 0.4.0'
